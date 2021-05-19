c8f3c118a4e891d0967849958aa8b099d8a49207
SYED-IZHAAN-S <syedizhaans.cse@citchennai.net>
2021-05-19 13:17:08 +0530

    homepage

---
+++ assets/admin/js/Fundorafunc.js
@@ -0,0 +1,44 @@
+const Laptops= document.getElementById('Laptops');
+// const Lap= document.getElementsById("Lap");
+
+const Mobiles= document.getElementById('Mobiles');
+// const Mob= document.getElementsById("Mob");
+
+const Accessories= document.getElementById('Accessories');
+// const Access= document.getElementsById("Access");
+
+
+
+
+Laptops.style.display="inline";
+Mobiles.style.display="none";
+Accessories.style.display="none";
+hAppliances.style.display="none";
+
+
+function laptops(){
+
+    Laptops.style.display="inline";
+    Mobiles.style.display="none";
+    Accessories.style.display="none";
+    hAppliances.style.display="none";
+
+}
+
+function mobiles(){
+
+    Laptops.style.display="none";
+    Mobiles.style.display="inline";
+    Accessories.style.display="none";
+    hAppliances.style.display="none";
+
+}
+
+function accessories(){
+
+    Laptops.style.display="none";
+    Mobiles.style.display="none";
+    Accessories.style.display="inline";
+    hAppliances.style.display="none";
+}
+
