969ea2bc9146a7e96be8b6d48f3ed14d03c37a25
Varshini K <78270481+Varshini-k-02@users.noreply.github.com>
2022-08-21 08:33:04 +0530

    Update settings.py

--- fundora/settings.py
+++ fundora/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$oc3jo_2qj%z7+2cp7nxe$cv+mld)t4(mzwj0cevmo!lxf4)1='
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
